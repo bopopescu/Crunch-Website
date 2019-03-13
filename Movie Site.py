bb07104e414e29cd4057b65fa23b482fadfe0518
Hershil Vora <hershil.vora17@stjohns.edu>
2019-03-13 12:33:56 +0000

    Movie Site.py edited online with Bitbucket

--- Movie Site.py
+++
@@ -1,9 +0,0 @@
-from app.Database import dbConnection
-from app import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': dbConnection()}
