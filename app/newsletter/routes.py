b220752fbae64e84a21e3761a81e45d43c8069fb
brendon-lei <brendonlei@gmail.com>
2019-04-03 16:07:37 -0400

    added attachment option to newsletter

--- app/newsletter/routes.py
+++ app/newsletter/routes.py
@@ -1,7 +1,20 @@
-from flask import render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from email.mime.text import MIMEText
-import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib, ssl, email
+import os
 from app.newsletter import newsletter_bp
+
+UPLOAD_FOLDER = '/Users/blei941/Documents/GitProjects/Testing' #change to fit where the code is located
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','.doc','.docx','.java','.py'])#add more if needed
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @newsletter_bp.route('/email') #starter page, would request subject/message body
 def email():
@@ -12,9 +25,12 @@ def email():
 def result():
     if request.method == 'POST':
         result = request.form
-        receiver_email = ["crunch@kwesi.info"] #gotta fill this up with the list of emails from the database
-        #gotta add a for loop to add emails
+        receiver_email = ["crunch@kwesi.info"] 
 
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         subject = result['subject']
         email = result['email']#message of the email
@@ -22,9 +38,19 @@ def result():
         for x in receiver_email: #allows to send emails to the array/list of emails
             sender_email = "crunch+test@kwesi.info"#this is our tester email for now
 
+            message = MIMEMultipart()
             text = """%s""" %(email)
+            message.attach(MIMEText(text,'plain'))
 
-            message = MIMEText(text)
+            with open(filename, "rb") as attachment:
+                # Add file as application/octet-stream
+                # Email client can usually download this automatically as attachment
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition',"attachment; filename= "+filename)
+            message.attach(part)
+
             message["Subject"] = subject
             message["From"] = sender_email
             message["To"] = x
@@ -35,5 +61,5 @@ def result():
                 server.sendmail(
                     sender_email, receiver_email, message.as_string()
                 )
-
+        os.remove(file.filename)
         return render_template('email_result.html')
